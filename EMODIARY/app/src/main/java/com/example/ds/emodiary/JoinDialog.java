479305e4062113a9fa1563396b4db88d4b7c519c
yeji9175 <yeji9175@naver.com>
2018-07-19 13:55:33 +0900

    no message

--- EMODIARY/app/src/main/java/com/example/ds/emodiary/JoinDialog.java
+++ EMODIARY/app/src/main/java/com/example/ds/emodiary/JoinDialog.java
@@ -4,16 +4,21 @@ import android.content.Context;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 /**
  * Created by jihu0 on 2018-06-26.
  */
 
 public class JoinDialog extends Dialog {
-
     Button Join;
+    EditText name, id, password, passwordCheck, phone;
 
     public JoinDialog(Context context){
         super(context);
@@ -23,12 +28,25 @@ public class JoinDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_dialog);
+
+        name = (EditText)findViewById(R.id.editName);
+        id = (EditText)findViewById(R.id.editId);
+        password = (EditText)findViewById(R.id.editPassword);
+        passwordCheck = (EditText)findViewById(R.id.editPasswordCheck);
+        phone = (EditText)findViewById(R.id.editPhone);
+
         Join = (Button)findViewById(R.id.Join);
         Join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(),MainActivity.class);
-                getContext().startActivity(intent);
+                //유효성 검사
+                if(!Patterns.EMAIL_ADDRESS.matcher(id.getText().toString()).matches()){
+                    Toast.makeText(getContext(), "아이디가 이메일 형식이 아닙니다",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent intent = new Intent(getContext(), MainActivity.class);
+                    getContext().startActivity(intent);
+                }
             }
         });
     }
