9783c496eee8f15d273be98230196f2f954511d8
yeji9175 <yeji9175@naver.com>
2018-07-29 16:48:46 +0900

    이젠 카메라가 되고 갤러리에서 가져오는게 안됨

--- EMODIARY/app/src/main/java/com/example/ds/emodiary/OneFragment.java
+++ EMODIARY/app/src/main/java/com/example/ds/emodiary/OneFragment.java
@@ -2,18 +2,20 @@ package com.example.ds.emodiary;
 
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
-
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.FileProvider;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,38 +25,30 @@ import android.widget.ImageView;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-
-/**
- * Created by jihu0 on 2018-06-26.
- */
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class OneFragment extends Fragment {
-
-    private static final int PICK_FROM_CAMERA=0;
-    private static final int PICK_FROM_ALBUM=1;
-    private static final int CROP_FROM_IMAGE=2;
-    private Uri mImageCaptureUri;
-    private ImageView iv_UserPhoto;
-    private int id_view;
-    private String absolutePath;
+    private final int GALLERY_CODE = 1111;
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private String imageFilePath;
+    private Uri photoUri;
+    private ImageView imageView;
+    private View view;
+    private Context context;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_1, container, false);
-
-        iv_UserPhoto = (ImageView)view.findViewById(R.id.user_image);
-        Button camera = (Button)view.findViewById(R.id.camera);
+        view = inflater.inflate(R.layout.fragment_1, container, false);
+        imageView = (ImageView)view.findViewById(R.id.user_image);
+        final Button camera = (Button)view.findViewById(R.id.camera);
+        context = view.getContext();
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //임시로 사용할 파일의 경로 생성
-                /*String url = "tmp_"+String.valueOf(System.currentTimeMillis())+".jpg";
-                mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),url));*/
-
-                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,mImageCaptureUri);
-                startActivityForResult(intent,PICK_FROM_CAMERA);
+                sendTakePhotoIntent();
             }
         });
 
@@ -62,67 +56,120 @@ public class OneFragment extends Fragment {
         album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //앨범에서 이미지 가져오기
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(intent, PICK_FROM_ALBUM);
+                startActivityForResult(intent, GALLERY_CODE);
             }
         });
-
         return view;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //if(resultCode != 1)
-        //    return;
 
-        switch(requestCode){
-            case PICK_FROM_ALBUM:
-            {
-                mImageCaptureUri = data.getData();
+        if(resultCode == getActivity().RESULT_OK){
+            switch (requestCode) {
+                case REQUEST_IMAGE_CAPTURE:
+                    Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+                    ExifInterface exif = null;
+
+                    try {
+                        exif = new ExifInterface(imageFilePath);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    int exifOrientation;
+                    int exifDegree;
+
+                    if (exif != null) {
+                        exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                        exifDegree = exifOrientationToDegrees(exifOrientation);
+                    } else {
+                        exifDegree = 0;
+                    }
+                    ((ImageView) view.findViewById(R.id.user_image)).setImageBitmap(rotate(bitmap, exifDegree));
+
+                case GALLERY_CODE:
+                    sendPicture(data.getData());
             }
-            case PICK_FROM_CAMERA:
-            {
-                //이미지를 가져온 후 리사이즈 할 이미지 크기 결정
-                Intent intent = new Intent("com.android.camera.action.CROP");
-                intent.setDataAndType(mImageCaptureUri,"image/*");
-
-                intent.putExtra("outputX",200);
-                intent.putExtra("outputY",200);
-                intent.putExtra("aspectX",1);
-                intent.putExtra("aspectY",1);
-                intent.putExtra("scale",true);
-                intent.putExtra("return-data",true);
-                startActivityForResult(intent,CROP_FROM_IMAGE);
-                break;
-            }
-            case CROP_FROM_IMAGE:
-            {
-                //크롭이 된 이후 이미지 넘겨받기
-                final Bundle extras = data.getExtras();
-
-                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel/"+System.currentTimeMillis()+".jpg";
-
-                if(extras != null){
-                    Bitmap photo = extras.getParcelable("data");
-                    iv_UserPhoto.setImageBitmap(photo);
-
-                    storeCropImage(photo,filePath);
-                    absolutePath = filePath;
-                    break;
-                }
-                File f = new File(mImageCaptureUri.getPath());
-                if(f.exists()){
-                    f.delete();
-                }
-            }
-
         }
+
     }
 
-    //비트맵 저장
+    private void sendPicture(Uri data) {
+        String imagePath = getRealPathFromURI(photoUri); // path 경로
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(imagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        int exifDegree = exifOrientationToDegrees(exifOrientation);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
+        imageView.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        int column_index=0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getActivity().getContentResolver().query(contentUri, proj, null, null, null);
+        if(cursor.moveToFirst()){
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+
+        return cursor.getString(column_index);
+    }
+
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private void sendTakePhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(context, getActivity().getPackageName(), photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,      /* prefix */
+                ".jpg",         /* suffix */
+                storageDir          /* directory */
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+    //지후
     private void storeCropImage(Bitmap bitmap, String filePath){
         String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
         File directory_SmartWheel = new File(dirPath);
@@ -147,5 +194,6 @@ public class OneFragment extends Fragment {
         }catch (Exception e){e.printStackTrace();}
 
     }
+
 
 }
